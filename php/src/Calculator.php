17d9b61d0ba86c3386ee0643f0731bafe5d53063
EdgarAGonz <90060701+EdgarAGonz@users.noreply.github.com>
2024-10-29 21:50:35 -0600

    Commit Update calculator

--- php/src/Calculator.php
+++ php/src/Calculator.php
@@ -1,11 +1,14 @@
 <?php
+
 // src/Calculator.php
 
-class Calculator {
+class Calculator
+{
     /**
      * Esta función toma dos números y devuelve su suma.
      */
-    public function add($a, $b) {
+    public function add($a, $b)
+    {
         return $a + $b;
     }
 }
